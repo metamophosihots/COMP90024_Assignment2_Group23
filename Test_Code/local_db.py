7fba613d326c61afe4ec3b116ed5d9ae1fbb0b17
hollywhy137 <80237395+hollywhy137@users.noreply.github.com>
2021-05-10 23:43:54 +1000

    test to use couchdb in localhost
    Signed-off-by: hollywhy137 <80237395+hollywhy137@users.noreply.github.com>

--- Test_Code/local_db.py
+++ Test_Code/local_db.py
@@ -1,5 +1,13 @@
 import couchdb
+import json
 
+"""
 couch = couchdb.Server('http://admin:admin@127.0.0.1:5984')
 db = couch.create('test')
+"""
+#with open('../tweets_mined.json') as file:
+with open('smallTwitter.json') as file:
+    jsonfile = json.load(file)
 
+data = jsonfile['tweets']
+print(data[3])
