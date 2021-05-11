7ae775a0be78f90b40f1aa79fcc8d15bcda2ab91
hollywhy137 <80237395+hollywhy137@users.noreply.github.com>
2021-05-11 15:56:58 +1000

    python write view document
    Signed-off-by: hollywhy137 <80237395+hollywhy137@users.noreply.github.com>

--- Test_Code/local_db.py
+++ Test_Code/local_db.py
@@ -1,13 +1,44 @@
 import couchdb
 import json
 
-"""
-couch = couchdb.Server('http://admin:admin@127.0.0.1:5984')
-db = couch.create('test')
-"""
-#with open('../tweets_mined.json') as file:
-with open('smallTwitter.json') as file:
-    jsonfile = json.load(file)
 
-data = jsonfile['tweets']
-print(data[3])
+# ignore this for now
+def load_json(path):
+    with open(path) as file:
+        content = json.load(file)
+    return content
+
+
+# this function writes the view document and store into the database
+def create_view(db, doc_name, view_name, map_function):
+    """
+    :param db: the database you want to save to
+    :param doc_name: name of the view document
+    :param view_name: name of the view
+    :param map_function: the map function, need to use mapreduce format
+    :return: None, will save the view document to database
+    """
+    data = {
+        "_id": f"_design/{doc_name}",
+        "views": {
+            view_name: {
+                "map": map_function
+            }
+        },
+        "language": "javascript",
+        "options": {"partitioned": False}
+    }
+    # logging.info( f"creating view {design_doc}/{view_name}" )
+    db.save(data)
+
+
+# this is the local couchdb server
+couch = couchdb.Server('http://admin:admin@127.0.0.1:5984')
+db = couch['test']  # assume databse 'test' already exist
+
+# a sample map function
+mapFunction = '''function (doc) {
+                      if(doc.retweet_count == 3)
+                      emit(doc.user_id, doc);
+                    }'''
+create_view(db, "python_view", "python_view", mapFunction)
