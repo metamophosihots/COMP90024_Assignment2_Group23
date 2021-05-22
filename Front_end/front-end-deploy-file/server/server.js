4caad94b51c2776fe1581de0f9eb8b5c34755c3a
KingDoubleSun <54192699+KingDoubleSun@users.noreply.github.com>
2021-05-23 01:17:29 +1000

    Fixed bug with server.js

--- Front_end/front-end-deploy-file/server/server.js
+++ Front_end/front-end-deploy-file/server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../my-dashboard/build")));
+app.use(express.static(path.join(__dirname, "../dashboard/build")));
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 
 const PORT = process.env.PORT || 3000;
